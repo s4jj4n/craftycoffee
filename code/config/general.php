ba983dd7aa96fd9a242361b2fe47dfead7efc9ce
Sajjan <s4jj4n@gmail.com>
2019-09-23 12:16:20 +0545

    plugin ckeditor installed

--- code/config/general.php
+++ code/config/general.php
@@ -18,7 +18,7 @@ return [
         'omitScriptNameInUrls' => true,
 
         // Control Panel trigger word
-        'cpTrigger' => 'admin',
+        'cpTrigger' => 'grinder',
 
         // The secure key Craft will use for hashing and encrypting data
         'securityKey' => getenv('SECURITY_KEY'),
